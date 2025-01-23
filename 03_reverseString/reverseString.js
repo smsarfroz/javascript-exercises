14119f6eaaa839102a1b9f410e242ad525d61537
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-23 13:14:52 +0000

    SHORTER WAY TO DO IT

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,24 @@
-const reverseString = function() {
+const reverseString = function(sentence) {
+    // let sentenceSize = sentence.length;
+    // let reversedSentence = "";
+    
+    // if(sentenceSize == 0) {
+    //     return '';
+    // }
+    // for(let i = sentenceSize-1 ;i >=0; --i) {
 
+    //     reversedSentence += sentence.charAt(i);
+    // }
+
+    // return reversedSentence;
+
+    return sentence.split('').reverse().join('');
+    /*
+    '' is used as seperator between the words
+    .split makes an array of characters from the string
+    .reverse reverses the array elements
+    .join joins them into a string 
+    */
 };
 
 // Do not edit below this line
