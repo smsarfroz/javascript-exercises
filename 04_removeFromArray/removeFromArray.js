f6f0393e9bd0d1ada77b0a71d22db625d4646a30
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-23 14:48:39 +0000

    APPLY FOREACH, INCLUDES, FILTER

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,43 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementsToRemove) {
+    let arrayAfterRemoval = [];
+    /*basic iteration
+    for(let i=0;i<array.length; ++i) {
+        if(!elementsToRemove.includes(array[i])) {
+            arrayAfterRemoval.push(array[i]);
+        }
+    }
+    */
+
+    /* forEach function for iteration
+    array.forEach((item) => {
+        if(!elementsToRemove.includes(item)){
+            arrayAfterRemoval.push(item);
+        }
+    });
+    */
+
+    //simple and advanced 
+    return array.filter((item) =>
+        !elementsToRemove.includes(item)
+    );
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+/*
+How do we do this ? 
+first of all how to define the arguments for the 
+function ? 
+we don't even know the number of arguments
+it would have ?
+there would be a function for taking arbitrary number of arguments 
+first argument must be array and its elements
+could be anything like integer or string
+
+but what is the intended complexity of the solution ? 
+like I can do it in O(n^2)
+for O(n), it would prolly need an inbuilt function, which is ? 
+
+*/
