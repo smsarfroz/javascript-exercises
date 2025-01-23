d824c9caabeac36fd7e443b96d6f64926d548572
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-23 14:59:49 +0000

    ITERATION

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(object1,object2) {
+    if(!Number.isInteger(object1)) {
+        return "ERROR";
+    }else {
+        if(object1 < 0) {
+            return "ERROR";
+        }
+    }
+    if(!Number.isInteger(object2)) {
+        return "ERROR";
+    }else {
+        if(object1 < 0) {
+            return "ERROR";
+        }
+    }
 
+
+    let result = 0;
+
+    for(let i = Math.min(object1,object2); i<=Math.max(object1,object2); ++i) {
+        result+=i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
