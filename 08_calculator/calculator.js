2c33a2d63bba5884e3612569ba3dde907ebd18e0
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-23 15:43:44 +0000

    CALCULATOR

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b ;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// let result = 0;
+  // array.forEach(element => {
+  //   result+=element;
+  // });
+
+  return array.reduce((total, current) => {
+    return (total+current);
+  },0);
+  // return result;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  // let result = 1 ;
 
+  // array.forEach(element => {
+  //   result*=element;
+  // });
+  // return result;
+
+  return array.reduce((result,current) => {
+    return result*current;
+  },1);
 };
 
-const power = function() {
+const power = function(a,b) {
 	
+  let result = 1;
+  for(let i=0;i<b; ++i) {
+    result *= a;
+  }
+  return result; 
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1; 
+  for(let i = a; i>=1; --i) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
