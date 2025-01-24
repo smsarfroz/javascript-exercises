d9b240baccab0577d3b81a3b7b5520afc32e827f
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-24 08:11:01 +0000

    FIND THE OLDEST

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,54 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = {
+        name : "",
+        age : 0
+    }
 
+    people.map((person) => {
+        if(person.yearOfDeath === undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        ageofPerson = person.yearOfDeath -
+        person.yearOfBirth;
+        if(ageofPerson > oldestPerson.age) {
+            oldestPerson = {
+                name: person.name,
+                age: ageofPerson
+            }
+        }
+        //what should we return now ? 
+        //isn't return necessary here ?
+        //map returns an array 
+        //we don't care about array here
+        return person; 
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+/*
+If death is undefined for some, 
+what do we do about it ? 
+should we find the age by assuming current year is 2025?
+
+or maybe Don't remain stuck
+and solve the simpler versions first
+
+so we know age, right 
+a variable that stores max age and a name associated
+with it
+to iterate, use map 
+
+if someone is not dead then is he automatically oldest ? 
+no, among all the living people 
+select the one whose birthyear is least 
+
+there is only one such person 
+no, age of this living person is still 
+presentyear - birthyear 
+
+
+*/
