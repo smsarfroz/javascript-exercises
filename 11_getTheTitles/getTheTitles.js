5e3ee5bd9ab9152f0e849063498cef3aab302514
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-01-24 07:21:51 +0000

    MAP THROUGH ARRAY OF OBJECTS

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return titles = books.map((bookObj) => {
+        return bookObj.title;
+    });
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+/*
+Okay, How to do this ? 
+we can iterate through objects in the books array
+then in the object select the title
+and push it into titles array
+
+iterate through map
+
+*/
